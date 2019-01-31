06582562ae03dce29f0e1ce055b6a990d10d30ef
“0rainge” <Orange96@aliyun.com>
2019-01-31 14:02:21 +0800

    feat(page view): Change the way data displayed

--- controllers/myToDoController.js
+++ controllers/myToDoController.js
@@ -1,6 +1,9 @@
+ var data = [{item:'好好学习' },{item:'锻炼身体'},{item:'作息规律'},{item:'早日暴富'},{item:'养大猫咪'}];
+ 
+ 
  module.exports = function(app){
      app.get('/todo',function(req, res){
-         res.render('myTodo');
+         res.render('myTodo',{flags: data});
      });
      app.post('/todo',function(req,res){
 
